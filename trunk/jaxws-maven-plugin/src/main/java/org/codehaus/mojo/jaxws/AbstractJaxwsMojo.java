dad4f9404493d02d831027465cf27cd7355f3885
kohsuke <kohsuke@3b447fe2-3421-0410-a1d7-a2ef89021db4>
2007-03-16 17:01:52 +0000

    importing jaxws-maven-plugin from codehaus mojo. Revision 3249

---
+++ trunk/jaxws-maven-plugin/src/main/java/org/codehaus/mojo/jaxws/AbstractJaxwsMojo.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2006 Codehaus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.codehaus.mojo.jaxws;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * 
+ * @author dantran <dantran@apache.org>
+ * @version $Id: AbstractJaxwsMojo.java 3240 2007-02-04 07:13:21Z dantran $ *
+ */
+
+public abstract class AbstractJaxwsMojo
+    extends AbstractMojo
+{
+
+    /**
+     * @parameter expression="${project}"
+     * @readonly
+     */
+    protected MavenProject project;
+
+    /**
+     * Specify where to place output generated classes 
+     * Set to "" to turn it off
+     * @parameter default-value="${project.build.outputDirectory}"
+     */
+    protected File destDir;
+
+    /**
+     * Output messages about what the tool is doing
+     * 
+     * @parameter default-value="false"
+     */
+    protected boolean verbose;
+
+    /**
+     * Keep generated files.
+     * 
+     * @parameter default-value="false"
+     */
+    protected boolean keep;
+
+    /**
+     * Allow to use the JAXWS Vendor Extensions.
+     * 
+     * @parameter default-value="false"
+     */
+    protected boolean extension;
+
+
+    /**
+     * Map of of plugin artifacts.
+     *
+     * @parameter expression="${plugin.artifactMap}"
+     * @readonly
+     */
+    private Map pluginArtifactMap;
+
+    /**
+     * Need to build a URLClassloader since Maven removed it form the chain
+     * @param parent
+     * @return
+     */
+    protected String initClassLoader( ClassLoader parent )
+        throws MojoExecutionException
+    {
+
+        try
+        {
+            List classpathFiles = project.getCompileClasspathElements();
+            
+            URL[] urls = new URL[classpathFiles.size() + 3];
+            
+            StringBuffer classPath = new StringBuffer();
+            
+            for ( int i = 0; i < classpathFiles.size(); ++i )
+            {
+                getLog().debug( (String) classpathFiles.get( i ) );
+                urls[i] = new File( (String) classpathFiles.get( i ) ).toURL();
+                classPath.append( (String) classpathFiles.get( i ) );
+                classPath.append( File.pathSeparatorChar );
+            }
+
+            
+            urls[classpathFiles.size()] = new File( project.getBuild().getOutputDirectory() ).toURL();
+
+            Artifact jaxwsToolsArtifact = (Artifact) pluginArtifactMap.get( "com.sun.xml.ws:jaxws-tools" );
+            urls[classpathFiles.size() + 1] = jaxwsToolsArtifact.getFile().toURL();
+            
+            File toolsJar = new File( System.getProperty( "java.home"), "../lib/tools.jar" );
+            if ( ! toolsJar.exists() ) 
+            {
+            	//
+            	toolsJar = new File( System.getProperty( "java.home"), "lib/tools.jar" );
+            }
+            urls[classpathFiles.size() + 2] = toolsJar.toURL();
+            
+            URLClassLoader cl = new URLClassLoader( urls, parent );
+
+            // Set the new classloader
+            Thread.currentThread().setContextClassLoader( cl );
+
+            System.setProperty( "java.class.path", classPath.toString() );
+
+            String sysCp = System.getProperty( "java.class.path" );
+
+            return sysCp;            
+        }
+        catch ( MalformedURLException e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+        catch ( DependencyResolutionRequiredException e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+
+    }
+}
---
+++ trunk/jaxws-maven-plugin/src/main/java/org/codehaus/mojo/jaxws/WsGenMojo.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2006 Guillaume Nodet.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.mojo.jaxws;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import com.sun.tools.ws.wscompile.WsgenTool;
+
+/**
+ * <p>
+ * A Maven 2 plugin which reads a service endpoint implementation class 
+ *   and generates all of the portable artifacts for a JAX-WS web service.
+ * </p>
+ * 
+ * @goal wsgen
+ * @phase generate-sources
+ * @requiresDependencyResolution
+ * @description JAXWS 2.x Plugin.
+ * @author gnodet <gnodet@apache.org>
+ * @author dantran <dantran@apache.org>
+ * @version $Id: WsGenMojo.java 3169 2007-01-22 02:51:29Z dantran $
+ */
+public class WsGenMojo
+    extends AbstractJaxwsMojo
+{
+
+    /**
+     * Specify that a WSDL file should be generated in ${resourceDestDir}
+     * 
+     * @parameter default-value="false"
+     */
+    private boolean genWsdl;
+
+    
+    /**
+     * Directory containing the generated wsdl files.
+     * 
+     * @parameter default-value="${project.build.directory}/jaxws/wsgen/wsdl"
+     */
+    
+    private File resourceDestDir;
+
+    /**
+     * service endpoint implementation class name.
+     * 
+     * @parameter 
+     * @required
+     */
+    private String sei;
+
+    /**
+     * Used in conjunction with genWsdl to specify the protocol to use in the 
+     * wsdl:binding.  Value values are "soap1.1" or "Xsoap1.2", default is "soap1.1". 
+     * "Xsoap1.2" is not standard and can only be used in conjunction with the 
+     * -extensions option
+     * 
+     * @parameter 
+     */
+    private String protocol;
+
+    
+    /**
+     * Specify where to place generated source files, keep is turned on with this option. 
+     * 
+     * @parameter 
+     */
+    private File sourceDestDir;
+    //default-value="${project.build.directory}/jaxws/java"
+    
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        init();
+
+        // Need to build a URLClassloader since Maven removed it form the chain
+        ClassLoader parent = this.getClass().getClassLoader();
+        String orginalSystemClasspath = this.initClassLoader( parent );
+
+        try
+        {
+            ArrayList<String> args = getWsGenArgs();
+            args.add( this.sei );
+
+            WsgenTool compTool = new WsgenTool( System.out );
+            if ( !compTool.run( args.toArray( new String[args.size()] ) ) )
+            {
+                throw new MojoExecutionException( "Error executing: wsimport " + args );
+            }
+            
+        }
+        finally
+        {
+            // Set back the old classloader
+            Thread.currentThread().setContextClassLoader( parent );
+            System.setProperty( "java.class.path", orginalSystemClasspath );
+        }
+    }
+
+    private void init()
+        throws MojoExecutionException, MojoFailureException
+    {
+        if ( !destDir.exists() )
+        {
+            destDir.mkdirs();
+        }
+    }
+
+    /**
+     * Construct wsgen arguments
+     * @return a list of arguments
+     * @throws MojoExecutionException
+     */
+    private ArrayList<String> getWsGenArgs()
+        throws MojoExecutionException
+    {
+        ArrayList<String> args = new ArrayList<String>();
+
+        if ( verbose )
+        {
+            args.add( "-verbose" );
+        }
+
+        if ( keep )
+        {
+            args.add( "-keep" );
+        }
+
+        if ( this.sourceDestDir != null )
+        {
+            args.add( "-s" );
+            args.add( this.sourceDestDir.getAbsolutePath() );
+            this.sourceDestDir.mkdirs();
+        }
+
+        args.add( "-d" );
+        args.add( this.destDir.getAbsolutePath() );
+        this.destDir.mkdirs();
+
+        if ( this.genWsdl )
+        {
+            if ( this.protocol != null )
+            {
+                args.add( "-wsdl:" + this.protocol );
+            }
+            else
+            {
+                args.add( "-wsdl" );
+            }
+
+            args.add( "-r" );
+            args.add( this.resourceDestDir.getAbsolutePath() );
+            this.resourceDestDir.mkdirs();
+
+        }
+
+        args.add( sei );
+
+        getLog().info( "jaxws:wsgen args: " + args );
+
+        return args;
+    }
+
+}
