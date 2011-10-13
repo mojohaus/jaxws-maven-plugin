4d8dd0b1cc9ff9ce1b9eda6c345bce5d8bf382b4
jungicz <jungicz@3b447fe2-3421-0410-a1d7-a2ef89021db4>
2011-10-13 15:35:39 +0000

    JAX_WS_COMMONS-38, JAX_WS_COMMONS-56: wsimport-test mojo

--- trunk/jaxws-maven-plugin/src/main/java/org/codehaus/mojo/jaxws/MainWsImportMojo.java
+++ trunk/jaxws-maven-plugin/src/main/java/org/codehaus/mojo/jaxws/MainWsImportMojo.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007-2011 JAX-WS RI team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.mojo.jaxws;
 
 import java.io.File;
@@ -13,6 +28,7 @@ import java.io.File;
  * @author Kohsuke Kawaguchi
  */
 public class MainWsImportMojo extends WsImportMojo {
+
     /**
      * Specify where to place output generated classes
      * Set to "" to turn it off
@@ -21,9 +37,25 @@ public class MainWsImportMojo extends WsImportMojo {
     protected File destDir;
 
     /**
+     * Specify where to place generated source files, keep is turned on with this option.
+     *
+     * @parameter default-value="${project.build.directory}/jaxws/wsimport/java"
+     */
+    private File sourceDestDir;
+
+    /**
      * Either ${build.outputDirectory} or ${build.testOutputDirectory}.
      */
     protected File getDestDir() {
         return destDir;
+    }
+
+    protected File getSourceDestDir() {
+        return sourceDestDir;
+    }
+
+    @Override
+    protected void addSourceRoot(String sourceDir) {
+        project.addCompileSourceRoot(sourceDir);
     }
 }
