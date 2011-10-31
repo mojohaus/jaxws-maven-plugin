356a6b953d90d8d93402a8e4380f38798a9c6963
jungicz <jungicz@3b447fe2-3421-0410-a1d7-a2ef89021db4>
2011-10-31 11:24:40 +0000

    JAX_WS_COMMONS-78: cleanup - licenses

--- src/main/java/org/jvnet/jax_ws_commons/jaxws/AbstractJaxwsMojo.java
+++ src/main/java/org/jvnet/jax_ws_commons/jaxws/AbstractJaxwsMojo.java
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.jvnet.jax_ws_commons.jaxws;
 
 import java.io.File;
--- src/main/java/org/jvnet/jax_ws_commons/jaxws/AbstractWsGenMojo.java
+++ src/main/java/org/jvnet/jax_ws_commons/jaxws/AbstractWsGenMojo.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jvnet.jax_ws_commons.jaxws;
 
 import com.sun.tools.ws.WsGen;
