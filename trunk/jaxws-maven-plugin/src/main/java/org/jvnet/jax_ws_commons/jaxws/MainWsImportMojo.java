27d69f10a37ba0247402b73b4f8bbefa2a5e1422
jungicz <jungicz@3b447fe2-3421-0410-a1d7-a2ef89021db4>
2012-02-21 15:04:27 +0000

    javadoc update

--- trunk/jaxws-maven-plugin/src/main/java/org/jvnet/jax_ws_commons/jaxws/MainWsImportMojo.java
+++ trunk/jaxws-maven-plugin/src/main/java/org/jvnet/jax_ws_commons/jaxws/MainWsImportMojo.java
@@ -30,8 +30,9 @@ import java.io.File;
 public class MainWsImportMojo extends WsImportMojo {
 
     /**
-     * Specify where to place output generated classes
-     * Set to "" to turn it off
+     * Specify where to place output generated classes. Use <code>xnocompile</code>
+     * to turn this off.
+     *
      * @parameter default-value="${project.build.outputDirectory}"
      */
     private File destDir;
