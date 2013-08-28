9fa062c4d96d0bdc18fee49fdbfacf4b0ea8a2ca
jungicz <jungicz@3b447fe2-3421-0410-a1d7-a2ef89021db4>
2013-08-28 17:03:48 +0000

    add dependency filtering (if needed)

---
+++ trunk/jaxws-maven-plugin/src/main/java/org/jvnet/jax_ws_commons/jaxws/ExclusionFilter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 Oracle.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jvnet.jax_ws_commons.jaxws;
+
+import java.util.List;
+import org.apache.maven.model.Exclusion;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.graph.DependencyFilter;
+import org.sonatype.aether.graph.DependencyNode;
+
+/**
+ *
+ * @author lukas
+ */
+final class ExclusionFilter implements DependencyFilter {
+
+    private final List<Exclusion> toExclude;
+
+    public ExclusionFilter(List<Exclusion> toExclude) {
+        assert toExclude != null : "Null is not allowed";
+        this.toExclude = toExclude;
+    }
+
+    @Override
+    public boolean accept(DependencyNode node, List<DependencyNode> parents) {
+        Artifact a = node.getDependency().getArtifact();
+        for (Exclusion e : toExclude) {
+            if (e.getGroupId().equals(a.getGroupId())
+                    && e.getArtifactId().equals(a.getArtifactId())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
