dd1d8a16a96e9c7ac4f82184708b8385bfb464b2
Hervé Boutemy <hboutemy@apache.org>
2015-11-09 23:27:51 +0100

    code clarification

--- src/main/java/org/codehaus/mojo/jaxws/Invoker.java
+++ src/main/java/org/codehaus/mojo/jaxws/Invoker.java
@@ -20,7 +20,6 @@ package org.codehaus.mojo.jaxws;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
@@ -42,68 +41,65 @@ public final class Invoker
     public static void main( String... args )
         throws Exception
     {
+        String toolClassname = args[0];
+
         int idx = 1;
-        String c = args[2];
+        String cp = args[2];
         if ( "-pathfile".equals( args[1] ) )
         {
-            Properties p = new Properties();
+            // load cp from properties file
             File pathFile = new File( args[2] );
             pathFile.deleteOnExit();
+            Properties p = new Properties();
             p.load( new FileInputStream( pathFile ) );
-            c = p.getProperty( "cp" );
+            cp = p.getProperty( "cp" );
             idx = 3;
         }
-        List<URL> cp = new ArrayList<URL>();
-        for ( String s : c.split( File.pathSeparator ) )
-        {
-            try
-            {
-                URL f = new File( s ).toURI().toURL();
-                cp.add( f );
-            }
-            catch ( MalformedURLException ex )
-            {
-                Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
-            }
-        }
-        URLClassLoader cl = new URLClassLoader( cp.toArray( new URL[cp.size()] ) );
-        String[] wsargs = new String[args.length - idx];
-        System.arraycopy( args, idx, wsargs, 0, args.length - idx );
+
+        URLClassLoader cl = new URLClassLoader( toUrls( cp ) );
+
+        // save original classloader and java.class.path property
         ClassLoader orig = Thread.currentThread().getContextClassLoader();
         String origJcp = System.getProperty( "java.class.path" );
+
+        // set to values for tool invocation
         Thread.currentThread().setContextClassLoader( cl );
-        System.setProperty( "java.class.path", c );
+        System.setProperty( "java.class.path", cp );
         try
         {
-            Class<?> compileTool = cl.loadClass( args[0] );
-            Constructor<?> ctor = compileTool.getConstructor( OutputStream.class );
-            Object tool = ctor.newInstance( System.out );
-            Method runMethod = compileTool.getMethod( "run", String[].class );
+            Class<?> toolClass = cl.loadClass( toolClassname );
+
+            Object tool = toolClass.getConstructor( OutputStream.class ).newInstance( System.out );
+            Method runMethod = toolClass.getMethod( "run", String[].class );
+
+            String[] wsargs = new String[args.length - idx];
+            System.arraycopy( args, idx, wsargs, 0, args.length - idx );
+
             System.exit( (Boolean) runMethod.invoke( tool, new Object[] { wsargs } ) ? 0 : 1 );
         }
         catch ( NoSuchMethodException ex )
         {
-            Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            logSevere( ex );
         }
         catch ( SecurityException ex )
         {
-            Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            logSevere( ex );
         }
         catch ( ClassNotFoundException ex )
         {
-            Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            logSevere( ex );
         }
         catch ( InstantiationException ex )
         {
-            Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            logSevere( ex );
         }
         catch ( IllegalAccessException ex )
         {
-            Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            logSevere( ex );
         }
         catch ( IllegalArgumentException ex )
         {
-            Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            logSevere( ex );
         }
         catch ( InvocationTargetException ex )
         {
@@ -116,5 +112,27 @@ public final class Invoker
             Thread.currentThread().setContextClassLoader( orig );
             System.setProperty( "java.class.path", origJcp );
         }
+    }
+
+    private static void logSevere( Exception ex )
+    {
+        Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+    }
+
+    private static URL[] toUrls( String c )
+    {
+        List<URL> urls = new ArrayList<URL>();
+        for ( String s : c.split( File.pathSeparator ) )
+        {
+            try
+            {
+                urls.add( new File( s ).toURI().toURL() );
+            }
+            catch ( MalformedURLException ex )
+            {
+                Logger.getLogger( Invoker.class.getName() ).log( Level.SEVERE, null, ex );
+            }
+        }
+        return urls.toArray( new URL[0] );
     }
 }
