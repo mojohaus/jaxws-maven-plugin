c157d23e5b6aa728062df2dd1a51e971e485c656
Petar Mitrovic <petarmitrovic@gmail.com>
2018-10-16 14:49:10 +0200

    SD-8167 Include the "endorsed" classpath along with regular one

--- src/main/java/org/codehaus/mojo/jaxws/AbstractJaxwsMojo.java
+++ src/main/java/org/codehaus/mojo/jaxws/AbstractJaxwsMojo.java
@@ -442,7 +442,7 @@ abstract class AbstractJaxwsMojo
             // Commented out since this arg is not supported anymore
             // cmd.createArg().setValue( "-Xbootclasspath/p:" + classpath.ecp );
             cmd.createArg().setValue( "-cp" );
-            cmd.createArg().setValue( classpath.invokerPath );
+            cmd.createArg().setValue( classpath.invokerPath + File.pathSeparator + classpath.ecp );
             cmd.createArg().setLine( Invoker.class.getCanonicalName() );
             cmd.createArg().setLine( getMain() );
             String extraCp = getExtraClasspath();
